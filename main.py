45a09edd1a80c7a086507d66812a207b067279f6
Aditya070204 <159102291+Aditya070204@users.noreply.github.com>
2026-03-31 17:44:15 +0530

    Refactor setup and shutdown for Redis integration
    Refactor setup and shutdown functions to include Redis initialization and closing.

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from sanic import Sanic
 from routes import bp
 from db import init_db
+from redis_client import init_redis, close_redis
 
 app = Sanic("BookAPI")
 
 app.blueprint(bp)
 
 @app.before_server_start
-async def setup_db(app):
+async def setup(app):
     await init_db()
+    await init_redis()
+
+@app.after_server_stop
+async def shutdown(app):
+    await close_redis()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=9999, debug=True, single_process=True)
