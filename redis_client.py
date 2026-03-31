7b5ec8fa3acadf0a84c56238a4ab2fb90cbd9b5d
Aditya070204 <159102291+Aditya070204@users.noreply.github.com>
2026-03-31 17:47:41 +0530

    Add files via upload

---
+++ redis_client.py
@@ -0,0 +1,16 @@
+import redis.asyncio as redis
+
+redis_client = None
+
+async def init_redis():
+    global redis_client
+    redis_client = redis.Redis(
+        host='localhost',
+        port = 6379,
+        decode_responses=True
+    )
+
+async def close_redis():
+    global redis_client
+    if redis_client:
+        await redis_client.close()
